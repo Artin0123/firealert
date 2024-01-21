1b2ddba8bd30d86348240b84eb7ed96f6303288f
Macarean <you@example.com>
2024-01-21 12:05:51 +0800

    test3

--- lib/main.dart
+++ lib/main.dart
@@ -377,79 +377,157 @@ class _Pagetwo extends State<PageTwo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //backgroundColor: const Color.fromARGB(240, 255, 255, 245),
-        body: SingleChildScrollView(
-            child: Column(children: <Widget>[
-      Card(
-        color: Colors.white38,
-        child: ListTile(
-          leading: Icon(
-            Icons.search,
-            color: Colors.blue,
-          ),
-          title: TextField(
-            //controller: controller,
-            decoration: InputDecoration(
-              hintText: '',
-              hintStyle: TextStyle(
-                color: Colors.blue,
-              ),
-              border: InputBorder.none,
-            ),
-          ),
-        ),
+        appBar: AppBar(title: Text('Search and Update Example'), actions: [
+      IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () async {
+            final String query = await showSearch(
+              context: context,
+              delegate: SearchBarDelegate(items),
+            );
+            if (query != null) {
+              filterItems(query);
+            }
+          })
+    ]) //backgroundColor: const Color.fromARGB(240, 255, 255, 245),
+        //     body: SingleChildScrollView(
+        //         child: Column(children: <Widget>[
+        //   Card(
+        //     color: Colors.white38,
+        //     child: ListTile(
+        //       leading: Icon(
+        //         Icons.search,
+        //         color: Colors.blue,
+        //       ),
+        //       title: TextField(
+        //         //controller: controller,
+        //         decoration: InputDecoration(
+        //           hintText: '',
+        //           hintStyle: TextStyle(
+        //             color: Colors.blue,
+        //           ),
+        //           border: InputBorder.none,
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        //   Card(
+        //       elevation: 6,
+        //       margin: const EdgeInsets.all(16),
+        //       color: const Color.fromARGB(255, 253, 208, 223),
+        //       shape: RoundedRectangleBorder(
+        //         borderRadius: BorderRadius.circular(10.0),
+        //         side: const BorderSide(
+        //           color: Color.fromARGB(248, 237, 127, 167),
+        //           width: 2.0,
+        //         ),
+        //       ),
+        //       child: Column(children: [
+        //         const ListTile(
+        //           title: Text('溫度感應器',
+        //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+        //         ),
+        //         Align(
+        //           alignment: Alignment.centerLeft,
+        //           child: Padding(
+        //             padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        //             child: Text('是否異常:\n感測器id :\n' '溫度: $temperatures\n',
+        //                 textAlign: TextAlign.left),
+        //           ),
+        //         ),
+        //       ])),
+        //   Card(
+        //       elevation: 6,
+        //       margin: const EdgeInsets.all(16),
+        //       color: Color.fromARGB(255, 208, 239, 253),
+        //       shape: RoundedRectangleBorder(
+        //         borderRadius: BorderRadius.circular(10.0),
+        //         side: const BorderSide(
+        //           color: Color.fromARGB(248, 127, 193, 237),
+        //           width: 2.0,
+        //         ),
+        //       ),
+        //       child: Column(children: [
+        //         const ListTile(
+        //           title: Text('空氣感應器',
+        //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+        //         ),
+        //         Align(
+        //           alignment: Alignment.centerLeft,
+        //           child: Padding(
+        //             padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        //             child: Text('是否異常:\n感測器id :\n' '氣體數值: $airqualitys\n',
+        //                 textAlign: TextAlign.left),
+        //           ),
+        //         ),
+        //       ])),
+        // ]))
+        );
+  }
+}
+
+class SearchBarDelegate extends SearchDelegate<String> {
+  final List<String> items;
+
+  SearchBarDelegate(this.items);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
       ),
-      Card(
-          elevation: 6,
-          margin: const EdgeInsets.all(16),
-          color: const Color.fromARGB(255, 253, 208, 223),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-            side: const BorderSide(
-              color: Color.fromARGB(248, 237, 127, 167),
-              width: 2.0,
-            ),
-          ),
-          child: Column(children: [
-            const ListTile(
-              title: Text('溫度感應器',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                child: Text('是否異常:\n感測器id :\n' '溫度: $temperatures\n',
-                    textAlign: TextAlign.left),
-              ),
-            ),
-          ])),
-      Card(
-          elevation: 6,
-          margin: const EdgeInsets.all(16),
-          color: Color.fromARGB(255, 208, 239, 253),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-            side: const BorderSide(
-              color: Color.fromARGB(248, 127, 193, 237),
-              width: 2.0,
-            ),
-          ),
-          child: Column(children: [
-            const ListTile(
-              title: Text('空氣感應器',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                child: Text('是否異常:\n感測器id :\n' '氣體數值: $airqualitys\n',
-                    textAlign: TextAlign.left),
-              ),
-            ),
-          ])),
-    ])));
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(items[index]),
+          onTap: () {
+            close(context, items[index]);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final List<String> suggestionList = query.isEmpty
+        ? items
+        : items
+            .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(suggestionList[index]),
+          onTap: () {
+            query = suggestionList[index];
+            showResults(context);
+          },
+        );
+      },
+    );
   }
 }
 
@@ -551,7 +629,7 @@ class NextPage extends StatelessWidget {
               child: TextFormField(
                 decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.person),
-                  labelText: "使用者名稱 *",
+                  labelText: "使用者名稱 ",
                   //hintText: "使用者名稱",
                 ),
               ),
@@ -562,7 +640,7 @@ class NextPage extends StatelessWidget {
                 decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.lock),
                   suffixIcon: Icon(Icons.remove_red_eye),
-                  labelText: "密碼 *",
+                  labelText: "密碼 ",
                   //hintText: "最好6個字",
                 ),
               ),
