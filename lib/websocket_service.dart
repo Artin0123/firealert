7f60ef9ebe8c1ebef5cb6c2cb95acadaa3af3b44
Macarean <you@example.com>
2024-02-14 13:29:09 +0800

    sensor

--- lib/websocket_service.dart
+++ lib/websocket_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutt/main.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:async';
@@ -8,6 +9,7 @@ import 'package:flutt/sensor_data.dart';
 
 class WebSocketService with ChangeNotifier {
   late IOWebSocketChannel _channel;
+  SensorData_list sensor_list = SensorData_list();
   String _connectionStatus = 'Disconnected';
   late StreamController<String> _messageController;
 
@@ -24,20 +26,27 @@ class WebSocketService with ChangeNotifier {
         var data = json.decode(message.toString());
         if (data['status'] == 'success') {
           _connectionStatus = 'Connected';
-          Map<String, dynamic> jsonData = jsonDecode(message);
-          String updatetime = jsonData['0_update_stamp'];
-          String isAlert = jsonData['alert'].toString();
-          List<dynamic> details = jsonData['details'];
-          for (var detail in details) {
-            String events = detail['event'];
-            String levels = detail['level'].toString();
-            String locations = detail['location'];
-            String timestamps = detail['time_stamp'];
-            Map<String, dynamic> sensors = detail['sensors'];
-            String airqualitys = sensors['air_quality'].toStringAsFixed(2);
-            String temperatures = sensors['temperature'].toStringAsFixed(2);
-          }
-
+          //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          // SensorData sensorData;
+          // Map<String, dynamic> jsonData = jsonDecode(message);
+          // String updatetime = jsonData['0_update_stamp'];
+          // String isAlert = jsonData['alert'].toString();
+          // List<dynamic> details = jsonData['details'];
+          // for (var detail in details) {
+          //   String events = detail['event'];
+          //   String levels = detail['level'].toString();
+          //   String locations = detail['location'];
+          //   String timestamps = detail['time_stamp'];
+          //   Map<String, dynamic> sensors = detail['sensors'];
+          //   String airqualitys = sensors['air_quality'].toStringAsFixed(2);
+          //   String temperatures = sensors['temperature'].toStringAsFixed(2);
+          // }
+          // String id = '123';
+          // String normals = 'Yes';
+          // sensorData = SensorData(airqualitys, temperatures, id, normals,
+          //     locations, events, isAlert, levels, updatetime);
+          // sensor_list.add(sensorData);
+          //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
           print('Connection established successfully');
           print(message);
         } else {
