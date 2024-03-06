c8fdbb0670d43d7ab89140502cc3e2cd9ecf01b7
Macarean <you@example.com>
2024-03-06 20:12:54 +0800

    刪掉

--- lib/local_notification_service.dart
+++ lib/local_notification_service.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:rxdart/subjects.dart';
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest.dart' as tz;
 
 class LocalNotificationService {
   LocalNotificationService();
@@ -11,7 +9,6 @@ class LocalNotificationService {
   final BehaviorSubject<String?> onNotificationClick = BehaviorSubject();
 
   Future<void> intialize() async {
-    tz.initializeTimeZones();
     const AndroidInitializationSettings androidInitializationSettings =
         AndroidInitializationSettings('@mipmap/ic_launcher');
 
